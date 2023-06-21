fed6df9cf6657af61371f00f05feb2cee4ec4212
Rodrigo Alejandro Ramos Lozano <85802575+rodrigocoris@users.noreply.github.com>
2023-06-20 18:16:29 -0600

    Add files via upload

---
+++ histograms-main/src/histograma/Server.java
@@ -0,0 +1,22 @@
+package histograma;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Server extends Remote {
+    void registerClient() throws RemoteException;
+
+    void uploadImage(byte[] image, String imageName) throws RemoteException;
+
+    byte[] getImageOne() throws RemoteException;
+
+    byte[] getImageTwo() throws RemoteException;
+
+    String getImageNameOne() throws RemoteException;
+
+    String getImageNameTwo() throws RemoteException;
+    
+    void setTime(String time, String tipo) throws RemoteException;
+   
+
+}
